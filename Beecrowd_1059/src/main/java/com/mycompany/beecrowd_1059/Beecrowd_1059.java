4867b8b1010ff5dad66c3c95cfe02cc09f778b86
EduardoGoncalves-02 <goncalvespereira02edu@gmail.com>
2026-04-27 11:20:20 -0300

    Exercício de while, números pares - 1059

---
+++ Beecrowd_1059/src/main/java/com/mycompany/beecrowd_1059/Beecrowd_1059.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.beecrowd_1059;
+
+/**
+ *
+ * @author Adri
+ */
+public class Beecrowd_1059 {
+
+    public static void main(String[] args) {
+        int x = 1;
+        while(x <= 100){
+            if(x % 2 == 0){
+                System.out.println(x);
+                x++;
+                
+            }
+            x++;
+        }
+    }
+}
